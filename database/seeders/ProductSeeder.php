18161292d7ca597c321f018ec7980b1ee28fcdc3
Tonmoy Deb Chowdhury <debtonmoy09@gmail.com>
2024-04-04 18:25:44 +0200

    adding product migration and seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 50%',
+            maxLtv: 50,
+            fee: 1,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 55%',
+            maxLtv: 55,
+            fee: 1.5,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 60%',
+            maxLtv: 60,
+            fee: 2,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 65%',
+            maxLtv: 65,
+            fee: 2.5,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 70%',
+            maxLtv: 70,
+            fee: 3,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 75%',
+            maxLtv: 75,
+            fee: 3.5,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Featured product with LTV 80%',
+            maxLtv: 80,
+            fee: 4,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Normal product with LTV 50%',
+            maxLtv: 50,
+            fee: 1,
+        );
+
+        $this->createFeaturedProduct(
+            name: 'Normal product with LTV 55%',
+            maxLtv: 55,
+            fee: 1.5,
+        );
+
+        $this->createProduct(
+            name: 'Normal product with LTV 60%',
+            maxLtv: 60,
+            fee: 2,
+        );
+
+        $this->createProduct(
+            name: 'Normal product with LTV 65%',
+            maxLtv: 65,
+            fee: 2.5,
+        );
+
+        $this->createProduct(
+            name: 'Normal product with LTV 70%',
+            maxLtv: 70,
+            fee: 3,
+        );
+
+        $this->createProduct(
+            name: 'Normal product with LTV 75%',
+            maxLtv: 75,
+            fee: 3.5,
+        );
+
+        $this->createProduct(
+            name: 'Normal product with LTV 80%',
+            maxLtv: 80,
+            fee: 4,
+        );
+    }
+
+    private function createFeaturedProduct(string $name, float $maxLtv, float $fee) {
+
+        Product::create([
+            'name' => $name,
+            'max_ltv' => $maxLtv,
+            'fee' => $fee,
+            'interest_rate' =>  $fee / 2,
+            'featured' => true,
+        ]);
+
+    }
+
+    private function createProduct(string $name, float $maxLtv, float $fee) {
+
+        Product::create([
+            'name' => $name,
+            'max_ltv' => $maxLtv,
+            'fee' => $fee,
+            'interest_rate' =>  $fee / 2,
+            'featured' => false,
+        ]);
+
+    }
+}
